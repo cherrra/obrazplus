29bc29cd55f2fbd4bd04b04c66b42149cc86fab7
cherrra <94289157+cherrra@users.noreply.github.com>
2025-06-14 17:06:10 +0300

    Create DatabaseService.java

---
+++ src/services/DatabaseService.java
@@ -0,0 +1,40 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseService {
+    private static DatabaseService instance;
+
+    private DatabaseService() throws SQLException {
+        //конструктор для паттерна Singleton
+    }
+
+    public static synchronized DatabaseService getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseService();
+        }
+        return instance;
+    }
+    //соединение бд
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL Driver not found.", e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/obraz_db";
+        String user = "root";
+        String password = "";
+
+        //каждый раз новое соединение
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    //заглушка
+    public void close() {
+        System.out.println("DatabaseService: Метод close() вызван. Соединения управляются индивидуально.");
+    }
+}
